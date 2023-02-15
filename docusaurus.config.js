b0ced08bdf044564d4201671a9af5f7929d2d155
Grace <95324995+i-grace@users.noreply.github.com>
2023-02-15 16:42:21 +1100

    Updated the baseURL to Fusion.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   url: 'https://datameshgroup.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/fusion-new',
+  baseUrl: '/fusion',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
