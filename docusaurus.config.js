a2591bea3ea7bb653d1b5b34497ab219487aa45a
Grace <95324995+i-grace@users.noreply.github.com>
2023-08-18 18:56:29 +1000

    Changed the date of update.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -82,7 +82,7 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: '2023-04-19',
+            title: '2023-08-18',
           },
           {
             title: 'Useful Links',
