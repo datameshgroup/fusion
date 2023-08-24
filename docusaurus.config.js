6f9441f4fe7ad0c8b939515697b02788f9b5db09
Grace <95324995+i-grace@users.noreply.github.com>
2023-08-24 12:39:45 +1000

    Added sample screens for when POS is paired with terminal and when POS is not yet paired with terminal.  Added Unpairing from Terminal section.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -82,7 +82,7 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: '2023-08-18',
+            title: '2023-08-24',
           },
           {
             title: 'Useful Links',
